48b6cf0c9c8e717a76d1f090f081bc2e07cb1a13
jzb <jiangzhongbo@doodlemobile.com>
2017-11-04 17:00:54 +0800

    add test

---
+++ Assets/Promise/test/PromiseTest.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+using UPromise;
+using System;
+public class PromiseTest {
+
+	[Test]
+    public void Test_then_immediately()
+    {
+        new Promise((a, b) =>
+        {
+            a(1);
+        }).Then(value =>
+        {
+            Assert.AreEqual(value, 1, "Then error");
+        });
+	}
+
+	// A UnityTest behaves like a coroutine in PlayMode
+	// and allows you to yield null to skip a frame in EditMode
+	[UnityTest]
+    public IEnumerator Test_then_later()
+    {
+        Promise.cb cb = null;
+        new Promise((a, b) =>
+        {
+            cb = a;
+        }).Then(value =>
+        {
+            Assert.AreEqual(value, 1, "Then error");
+        });
+        yield return later(5);
+        cb(1);
+	}
+
+    IEnumerator later(float t, Action func = null)
+    {
+        yield return new WaitForSeconds(t);
+        if(func != null) func();
+    }
+}
