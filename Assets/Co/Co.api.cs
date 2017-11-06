553bad1cab0f8db8133711d33b739ecacaade164
jzb <jiangzhongbo@doodlemobile.com>
2017-11-06 23:14:52 +0800

    edit co

---
+++ Assets/Co/Co.api.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public partial class Co
+{
+    public void With(PoolType t)
+    {
+        poolType = t;
+        if (t == PoolType.Sequence)
+        {
+            pool = new SequencePool();
+        }
+        else if (t == PoolType.Concurrent)
+        {
+            pool = new ConcurrentPool();
+        }
+    }
+
+    public PoolType GetPoolType()
+    {
+        return poolType;
+    }
+
+    public Coroutine Run(Coroutine co)
+    {
+        if (outer_inter.ContainsKey(co))
+        {
+            pool.Add(outer_inter[co]);
+        }
+        return co;
+    }
+
+    public Coroutine Run(IEnumerator ie, RunType type = RunType.Update)
+    {
+        return Run(Create(ie, type));
+    }
+
+    public CoroutineState Status(Coroutine co)
+    {
+        if (outer_inter.ContainsKey(co))
+        {
+            return outer_inter[co].State;
+        }
+        return CoroutineState.Dead;
+    }
+
+    public Coroutine Create(IEnumerator ie, RunType type = RunType.Update)
+    {
+        Coroutine c = new Coroutine();
+        _Coroutine _c = create(ie, type);
+        outer_inter[c] = _c;
+        inter_outer[_c] = c;
+        return c;
+    }
+
+    public Coroutine Running()
+    {
+        if(co_current == null || !inter_outer.ContainsKey(co_current))
+        {
+            return null;
+        }
+        return inter_outer[co_current];
+    }
+}
---
+++ Assets/Co/Co.debug.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using UPromise;
+public partial class Co
+{
+
+}
