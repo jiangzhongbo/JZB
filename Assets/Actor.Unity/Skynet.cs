b5c65b6e560b68781aae746eeba85786a7248f73
jzb <jiangzhongbo@gmail.com>
2017-11-08 21:15:18 +0800

    edit

---
+++ Assets/Actor.Unity/Skynet.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+namespace UActor
+{
+    public class Skynet : MonoBehaviour
+    {
+        private struct skynet_message
+        {
+            public int source;
+            public int dest;
+            public int session;
+            public int type;
+            public string func_name;
+            public object[] args;
+            public Delegate fn;
+        }
+
+        private static Dictionary<int, Queue<skynet_message>> Q = new Dictionary<int, Queue<skynet_message>>();
+
+        public static int Send(
+            int source, 
+            int dest, 
+            int session, 
+            int type, 
+            string func_name, 
+            params object[] args
+            )
+        {
+            return 0;
+        }
+
+        public static int Send<T>(
+            int source,
+            int dest,
+            int session,
+            int type,
+            Func<T, IEnumerator> fn
+            ) where T : Actor<IDispatch>
+        {
+            return 0;
+        }
+
+        public static int Timeout(float delay, Action fn)
+        {
+            return 0;
+        }
+
+        public static Action CancalableTimeout(float delay, Action fn)
+        {
+            return () => { };
+        }
+    }
+}
---
+++ Assets/Actor.Unity/TestActor.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UActor;
+using System;
+public class TestActor : Actor<Sequence>
+{
+    public override IEnumerator Init()
+    {
+        yield return 
+            Ask(() => { return A(0); })
+            .Then(value =>
+            {
+                return Ask(() => { return A((int)value); });
+            });
+    }
+
+    public IEnumerator A(int value)
+    {
+        yield return new WaitForSeconds(10);
+        Reply(value + 1);
+        yield return null;
+    }
+
+    public IEnumerator B(int value)
+    {
+        Reply(value + 1);
+        yield return null;
+    }
+}
